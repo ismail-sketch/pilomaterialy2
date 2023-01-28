97745c5346b35d288b3183313f2ccb2d01b2a32c
Gennadiy <gennadiy.mutalibov@gmail.com>
2023-01-28 16:43:01 +0300

    сайт готов

--- js/index.js
+++ js/index.js
@@ -36,3 +36,37 @@ function moveContacts() {
 moveContacts();
 
 window.addEventListener('resize', moveContacts);
+
+// Добавить бордер хедеру================================
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 600) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+})
+
+// PopUp с формой===============================
+const overlay = document.querySelector('.overlay');
+const cross = document.querySelector('.cross');
+const popupBtns = document.querySelectorAll('.popup-btn');
+
+popupBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        overlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    })
+})
+if(cross !== null && cross !== undefined) {
+    cross.addEventListener('click', (e) => {
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    })
+}
+
+overlay.addEventListener('click', (e) => {
+    if(!e.target.closest('.form')) {
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+})
